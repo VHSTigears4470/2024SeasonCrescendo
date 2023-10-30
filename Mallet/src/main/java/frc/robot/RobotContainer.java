fcec73aaa93f1f5bacde3a58e401a943c7ebdb70
phomebrew <stevenhsiao4@gmail.com>
2023-10-29 20:58:30 -0700

    Swerve Drive Config And X-Box Controller Updated swerve drive config to have proper ids and gyroscope, still need to configure encoder though. Also deleted joystick controllers and replaced them with x-box controllers

--- Mallet/src/main/java/frc/robot/RobotContainer.java
+++ Mallet/src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,8 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -12,11 +11,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.drivebase.AbsoluteDrive;
-import frc.robot.subsystems.*;
 
 import java.io.File;
 import java.util.HashMap;
@@ -25,9 +22,8 @@ public class RobotContainer {
   // INIT SUBSYSTEMS
   private static final Drivetrain m_drivetrain = new Drivetrain(new File(Filesystem.getDeployDirectory(), "neo/swerve"));
   
-  // INIT JOYSTICKS (NOTE: PLEASE RENAME TO LEFT/RIGHT)
-  public static Joystick m_controller_arm = new Joystick(0);
-  public static Joystick m_controller_drive = new Joystick(1);
+  // INIT XBOX CONTROLLER
+  public static XboxController m_xbox = new XboxController(0);
 
   // INIT JOYSTICK ARRAYS
   public static HashMap<String, Trigger> controllerButtons_arm = new HashMap<String, Trigger>();
@@ -42,7 +38,7 @@ public class RobotContainer {
 
   public RobotContainer() {
     configureButtonBindings();
-    m_drivetrain.setDefaultCommand(new AbsoluteDrive(m_drivetrain, () -> MathUtil.applyDeadband(m_controller_drive.getRawAxis(0), OperatorConstants.LEFT_X_DEADBAND), () -> MathUtil.applyDeadband(m_controller_drive.getRawAxis(1), OperatorConstants.LEFT_Y_DEADBAND), () -> m_controller_arm.getRawAxis(0), () -> m_controller_arm.getRawAxis(1), false));
+    m_drivetrain.setDefaultCommand(new AbsoluteDrive(m_drivetrain, () -> MathUtil.applyDeadband(m_xbox.getLeftY(), OperatorConstants.LEFT_X_DEADBAND), () -> MathUtil.applyDeadband(m_xbox.getLeftX(), OperatorConstants.LEFT_Y_DEADBAND), () -> m_xbox.getRightX(), () -> m_xbox.getRightY(), false));
     String[] autoList = {"Do Nothing"};
     SmartDashboard.putStringArray("Auto List", autoList);
     initializeAutoChooser();
@@ -61,27 +57,9 @@ public class RobotContainer {
   }
 
   // assign button functions
-  private void configureButtonBindings() {
-    
-    // Add joystick buttons to maps
-    controllerButtons_drive.put("trigger", new JoystickButton(m_controller_drive, 1));
-    controllerButtons_arm.put("trigger", new JoystickButton(m_controller_arm, 1));
-    for (int i = 1; i <= 11; i++)
-    {
-      controllerButtons_arm.put(Integer.toString(i), new JoystickButton(m_controller_arm, i));
-      controllerButtons_drive.put(Integer.toString(i), new JoystickButton(m_controller_drive, i));
-    }
-  }
+  private void configureButtonBindings() {}
 
   public Command getAutoInput() {
     return m_autoChooser.getSelected();
-  }
-
-  public static Joystick getDriveController() {
-    return m_controller_drive;
-  }
-
-  public static Joystick getArmController() {
-    return m_controller_arm;
   }
 }
