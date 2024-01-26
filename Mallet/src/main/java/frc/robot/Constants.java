c52f880fd91b47a0b39ba50b2586ded6c419462a
overlordpro-sys <brent.littlebean@gmail.com>
2024-01-25 18:19:51 -0800

    Removed old arcade drive constants

--- Mallet/src/main/java/frc/robot/Constants.java
+++ Mallet/src/main/java/frc/robot/Constants.java
@@ -1,24 +1,12 @@
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpWi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import swervelib.math.Matter;
 import swervelib.parser.PIDFConfig;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
 public final class Constants {
-    public static final double K_SPEED = 0.5; // speed for turning
-    public static final double K_WHEEL_DIAMETER_INCH = 6; // diameter of the wheels in inches
-    public static final double K_TICKS_PER_FEET = 40.964489;
-
-    /*----------
-    AUTO
-    -----------*/
-    public static final double K_FORWARDS_FEET = 9 * K_TICKS_PER_FEET; // 17.666666
-    public static final double K_BACKWARDS_FEET = 6 * K_TICKS_PER_FEET; // 6.572500
-    public static final float K_TURN_ERROR_RANGE = 2f;
-    public static final double K_DEC_TO_PI = 0.01745;
-
     public static final class SwerveConstants {
         public static final PIDFConfig X_AUTO_PID = new PIDFConfig(0.000000005, 0, 0);
         public static final PIDFConfig Y_AUTO_PID = new PIDFConfig(0.000000005, 0, 0);
