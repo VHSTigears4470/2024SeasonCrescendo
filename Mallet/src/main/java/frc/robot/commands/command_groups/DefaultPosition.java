52ad59472e016c150bbc56236e518dcde8331eff
overlordpro-sys <brent.littlebean@gmail.com>
2024-02-07 17:14:23 -0800

    Added ElevatorSetHeightState usage as example with ENUM

--- Mallet/src/main/java/frc/robot/commands/command_groups/DefaultPosition.java
+++ Mallet/src/main/java/frc/robot/commands/command_groups/DefaultPosition.java
@@ -1,3 +1,5 @@
+package frc.robot.commands.command_groups;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
--- Mallet/src/main/java/frc/robot/commands/command_groups/IntakeNoteCommandGroup.java
+++ Mallet/src/main/java/frc/robot/commands/command_groups/IntakeNoteCommandGroup.java
@@ -1,17 +1,16 @@
+package frc.robot.commands.command_groups;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.ElevatorConstants.ELEVATOR_STATE;
+import frc.robot.commands.elevator.ElevatorSetHeightState;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class IntakeNoteCommandGroup extends SequentialCommandGroup {
     public IntakeNoteCommandGroup(IntakeSubsystem intakeSubsystem, ElevatorSubsystem elevatorSubsystem) {
-        addCommands (
-            intakeSubsystem.retractPusherCommand()
-            elevatorSubsystem.setHeightStateCommand(DOWN);
-
-        );
-    
-        
-            intakeSubsystem.retractPusherCommand();
-                   );
+        addCommands(
+                intakeSubsystem.retractPusherCommand(),
+                new ElevatorSetHeightState(elevatorSubsystem, ELEVATOR_STATE.DOWN),
+                intakeSubsystem.retractPusherCommand());
     }
 }
