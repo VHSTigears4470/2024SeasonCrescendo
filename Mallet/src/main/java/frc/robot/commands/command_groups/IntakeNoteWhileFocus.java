cac9a828f05fd8de0a1021c9091dce10a4f71d7a
4470 <vhsteam4470@gmail.com>
2024-03-27 16:17:54 -0700

    Fixed IntakeAndSuck

--- Mallet/src/main/java/frc/robot/commands/command_groups/IntakeNoteWhileFocus.java
+++ Mallet/src/main/java/frc/robot/commands/command_groups/IntakeNoteWhileFocus.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.command_groups;
 
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -13,24 +14,31 @@ import frc.robot.subsystems.NoteLimelight;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class IntakeNoteWhileFocus extends SequentialCommandGroup {
-        public IntakeNoteWhileFocus(SwerveSubsystem swerveSubsystem, IntakeSubsystem intakeSubsystem,
-                        ElevatorSubsystem elevatorSubsystem, NoteLimelight limelight) {
-                // Does nothing if a note is already in the intake
-                if (intakeSubsystem.noteBreambeamTripped()) {
-                        addCommands(
-                                        // Centers while going to intake position, wait command used to ensure enough
-                                        // time to set up intake position
-                                        new ParallelCommandGroup(
-                                                        // Sets robot to intake position
-                                                        new IntakePosition(intakeSubsystem, elevatorSubsystem),
-                                                        // Centers robot before going
-                                                        new FocusNote(swerveSubsystem, limelight)),
-                                        // Go forward with adjustments until robot is gets a note
-                                        new ParallelRaceGroup(
-                                                        new IntakeSetIntakeVoltageEndWithBreakbeam(intakeSubsystem),
-                                                        new DriveForwardWithFocusNote(swerveSubsystem, limelight),
-                                                        new WaitCommand(2) // Deadline
-                                        ));
-                }
-        }
+    public IntakeNoteWhileFocus(SwerveSubsystem swerveSubsystem, IntakeSubsystem intakeSubsystem,
+            ElevatorSubsystem elevatorSubsystem, NoteLimelight limelight) {
+        // Does nothing if a note is already in the intake
+        addCommands(
+                new ConditionalCommand(
+                        new SequentialCommandGroup(new ParallelCommandGroup(
+                                // Sets robot to intake position
+                                new IntakePosition(intakeSubsystem, elevatorSubsystem),
+                                // Centers robot before going
+                                new FocusNote(swerveSubsystem, limelight)),
+                                // Go forward with adjustments until robot is gets a
+                                // note
+                                new ParallelRaceGroup(
+                                        new IntakeSetIntakeVoltageEndWithBreakbeam(
+                                                intakeSubsystem),
+                                        new DriveForwardWithFocusNote(
+                                                swerveSubsystem,
+                                                limelight)),
+                                new WaitCommand(2) // Deadline
+                        ),
+                        new WaitCommand(0),
+                        () -> !intakeSubsystem.noteBreambeamTripped())
+        // Centers while going to intake position, wait command used to ensure enough
+        // time to set up intake position
+        );
+
+    }
 }
